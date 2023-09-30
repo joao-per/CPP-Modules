2c5dd6f95d9235d2488f3dd655c45183c10840f7
joao-per <rodriguespereirajoaopaulo@gmail.com>
2023-09-30 14:40:45 +0100

    Updated Syntax on ex00

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -1,17 +1,15 @@
-#include <iostream>
-#include <string>
 #include "whatever.hpp"
+#include <iostream>
 
 int main( void )
 {
 	int a = 2;
 	int b = 3;
 
-	::swap( a, b );
+	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
@@ -19,5 +17,5 @@ int main( void )
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
-	return 0;
+	return (0);
 }
