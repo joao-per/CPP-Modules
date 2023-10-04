2da7d0ad1ed4472a00841c56b3e6b3f03fb2cdb1
João Pereira <109032016+joao-per@users.noreply.github.com>
2023-10-04 10:52:51 +0000

    Ended RPN exercise

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -8,36 +8,54 @@
 
 int RPN::performOperation(int a, int b, char op)
 {
-    switch (op)
-    {
-        case '+':
-            return a + b;
-        case '-':
-            return a - b;
-        case '*':
-            return a * b;
-        case '/':
-            if (b == 0)
-                throw std::runtime_error("Division by 0.");
-            return a / b;
-        default:
-            throw std::runtime_error("Invalid operator.");
-    }
+	switch (op)
+	{
+		case '+':
+			return a + b;
+		case '-':
+			return a - b;
+		case '*':
+			return a * b;
+		case '/':
+			if (b == 0)
+				throw std::runtime_error("Division by 0.");
+			return a / b;
+		default:
+			throw std::runtime_error("Invalid operator.");
+	}
 }
 
 int RPN::calculateRPN(const std::string& expression)
 {
-    std::string token;
-    std::istringstream iss(expression);
+	std::string token;
+	std::istringstream iss(expression);
 
-    while (iss >> token)
-    {
-        if (isdigit(token[0]))
-        {
-            operands.push(std::atoi(token.c_str()));
-            std::cout << "Pushed " << token << std::endl;
-        }
+	while (iss >> token)
+	{
+		if (isdigit(token[0]))
+		{
+			operands.push(std::atoi(token.c_str()));
+			//std::cout << "Pushed " << token << std::endl;
+		}
+		else if (token.size() == 1 && std::string("+-*/").find(token[0]) != std::string::npos)
+		{
+			if (operands.size() < 2)
+				throw std::runtime_error("Not enough operands.");
+			// Pop two operands from the stack
+			int operand2 = operands.top(); operands.pop();
+			int operand1 = operands.top(); operands.pop();
 
-    }
-    return 0;
+			// Perform operation and push the result back
+			operands.push(performOperation(operand1, operand2, token[0]));
+			//std::cout << "Pushed " << performOperation(operand1, operand2, token[0]) << std::endl;
+		}
+		else
+			throw std::runtime_error("Invalid token.");
+
+	}
+
+	if(operands.size() != 1)
+		throw std::runtime_error("Invalid expression.");
+
+	return (operands.top());
 }
